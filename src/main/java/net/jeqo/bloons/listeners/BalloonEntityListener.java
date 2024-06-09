2a75c9a51320adf8ff9ce8d64059c52ddb2307db
Ian Tapply <contact@ian-tapply.me>
2024-06-09 19:08:23 -0400

    feat: Version 1.1.2-BETA - Nether and Config Fixes (#23)

---
+++ src/main/java/net/jeqo/bloons/listeners/BalloonEntityListener.java
@@ -0,0 +1,21 @@
+package net.jeqo.bloons.listeners;
+
+import net.jeqo.bloons.configuration.BalloonConfiguration;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityPortalEvent;
+
+public class BalloonEntityListener implements Listener {
+
+    /**
+     * Stop the chicken from going through the portal to prevent unleashing from player to balloon
+     * @param event The event that is called when an entity goes through a portal
+     */
+    @EventHandler
+    public void onChickenPortalLeave(EntityPortalEvent event) {
+        if (event.getEntity().getCustomName() != null && event.getEntity().getCustomName().contains(BalloonConfiguration.BALLOON_CHICKEN_ID)) {
+            event.setCancelled(true);
+        }
+    }
+
+}
