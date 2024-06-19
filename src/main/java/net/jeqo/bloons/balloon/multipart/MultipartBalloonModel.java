a3a0e4c8e13303e0bf49240e652368cf22c22d2d
IanTapply22 <contact@ian-tapply.me>
2024-06-18 22:08:29 -0400

    Started javadoc commenting of MultipartBalloonType class instancing methods

--- src/main/java/net/jeqo/bloons/balloon/multipart/MultipartBalloonModel.java
+++ src/main/java/net/jeqo/bloons/balloon/multipart/MultipartBalloonModel.java
@@ -21,11 +21,11 @@ public class MultipartBalloonModel {
     private Integer customModelData;
 
     /**
-     * Creates a new model for a multipart balloon
-     * @param modelType The type of model (head, body, tail)
-     * @param material The material of the model as a string
-     * @param color The color of the model as a hex color code value
-     * @param customModelData The custom model data value as a integer
+     *                          Creates a new model for a multipart balloon
+     * @param modelType         The type of model (head, body, tail), type net.jeqo.bloons.balloon.model.BalloonModelType
+     * @param material          The name of the Bukkit Material used to create the item, type java.lang.String
+     * @param color             The color of the model as a hex color code value, type java.lang.String
+     * @param customModelData   The custom model data value stored in the item metadata, type int
      */
     public MultipartBalloonModel(BalloonModelType modelType, String material, String color, int customModelData) {
         this.setModelType(modelType);
@@ -35,33 +35,45 @@ public class MultipartBalloonModel {
     }
 
     /**
-     * Gets the finalized item of a multipart balloon model
-     * @return The finalized item as an ItemStack
+     *              Gets the finalized item of a multipart balloon model with the specified metadata
+     * @return      The finalized item model with the specified metadata, type org.bukkit.inventory.ItemStack
      */
     public ItemStack getFinalizedModel() {
+        // Gets the Bukkit Material type from the string
         Material material = Material.getMaterial(this.getMaterial().toUpperCase());
+
+        // Check if the material is valid
         if (material == null) {
             Logger.logError("Material " + this.getMaterial() + " is not a valid material.");
             return null;
         }
 
+        // Check if the color is null and the custom model data is null, return only the raw ItemStack
         if (this.getColor() == null && this.getCustomModelData() == null) {
             return new ItemStack(material);
+
+        // Check if the color is null and the custom model data is not null, return the model with the custom model data
         } else if (this.getColor() == null) {
             return BalloonModel.createBlankModel(material, this.getCustomModelData());
+
+        // If everything provided isn't null and is valid, create the model with the specified color and custom model data
         } else if (this.getColor().startsWith("#")) {
-            // Check if valid hex code
+            // Check if valid hex code via the utility in net.jeqo.bloons.utils.ColorManagement
             if (!ColorManagement.isHexCode(this.getColor())) {
                 Logger.logError("Color " + this.getColor() + " is not a valid hex code.");
                 return null;
             }
 
+            // Convert the hex code to a valid org.bukkit.Color object
             Color color = ColorManagement.hexToColor(this.getColor());
             return BalloonModel.createColouredModel(material, color, this.getCustomModelData());
+
+        // If the color is not a hex code and there is no custom model data, only return a raw org.bukkit.inventory.ItemStack
         } else if (this.getCustomModelData() == null) {
             return new ItemStack(material);
         }
 
+        // Return null if otherwise nothing is specified
         return null;
     }
 }
--- src/main/java/net/jeqo/bloons/balloon/multipart/MultipartBalloonType.java
+++ src/main/java/net/jeqo/bloons/balloon/multipart/MultipartBalloonType.java
@@ -8,7 +8,6 @@ import lombok.Setter;
  */
 @Getter @Setter
 public class MultipartBalloonType {
-
     private String id;
     private String permission;
     private String name;
@@ -28,6 +27,28 @@ public class MultipartBalloonType {
     private MultipartBalloonModel bodyModel;
     private MultipartBalloonModel tailModel;
 
+    /**
+     *                                      Creates a new multipart balloon type which contains
+     *                                      the data in the configuration for the balloon
+     * @param id                            The id of the balloon, type java.lang.String
+     * @param permission                    The permission required to use the balloon (i.e. blue.jeqo), type java.lang.String
+     * @param name                          The name of the balloon, type java.lang.String
+     * @param lore                          The lore lines of the balloon in the balloon GUI, type java.lang.String[]
+     * @param nodeCount                     The number of nodes, or model, in the balloon, type int
+     * @param distanceBetweenNodes          The distance between each node in the balloon measured as blocks, type double
+     * @param headNodeOffset                The offset of the head node from its 0 position measured as blocks, type double
+     * @param bodyNodeOffset                The offset of the body node from its 0 position measured as blocks, type double
+     * @param tailNodeOffset                The offset of the tail node from its 0 position measured as blocks, type double
+     * @param maxNodeJointAngle             The maximum angle a node can rotate in degrees, type double
+     * @param yAxisInterpolation            The interpolation of the Y-axis, type double
+     * @param turningSplineInterpolation    The interpolation of the turning spline to prevent overturning, type double
+     * @param passiveSineWaveSpeed          The speed of the passive sine wave animation, type double
+     * @param passiveSineWaveAmplitude      The amplitude of the passive sine wave animation, type double
+     * @param passiveNoseSineWaveAmplitude  The amplitude of the passive sine wave animation starting at the nose, type double
+     * @param headModel                     The model used for the head node, type net.jeqo.bloons.balloon.multipart.MultipartBalloonModel
+     * @param bodyModel                     The model used for the body node, type net.jeqo.bloons.balloon.multipart.MultipartBalloonModel
+     * @param tailModel                     The model used for the tail node, type net.jeqo.bloons.balloon.multipart.MultipartBalloonModel
+     */
     public MultipartBalloonType(String id, String permission, String name, String[] lore, int nodeCount, double distanceBetweenNodes, double headNodeOffset, double bodyNodeOffset, double tailNodeOffset, double maxNodeJointAngle, double yAxisInterpolation, double turningSplineInterpolation, double passiveSineWaveSpeed, double passiveSineWaveAmplitude, double passiveNoseSineWaveAmplitude, MultipartBalloonModel headModel, MultipartBalloonModel bodyModel, MultipartBalloonModel tailModel) {
         this.setId(id);
         this.setPermission(permission);
@@ -49,6 +70,27 @@ public class MultipartBalloonType {
         this.setTailModel(tailModel);
     }
 
+    /**
+     *                                     Creates a new multipart balloon type which contains
+     *                                     the data in the configuration for the balloon
+     * @param id                           The id of the balloon, type java.lang.String
+     * @param permission                   The permission required to use the balloon (i.e. blue.jeqo), type java.lang.String
+     * @param name                         The name of the balloon, type java.lang.String
+     * @param lore                         The lore lines of the balloon in the balloon GUI, type java.lang.String[]
+     * @param nodeCount                    The number of nodes, or model, in the balloon, type int
+     * @param distanceBetweenNodes         The distance between each node in the balloon measured as blocks, type double
+     * @param headNodeOffset               The offset of the head node from its 0 position measured as blocks, type double
+     * @param bodyNodeOffset               The offset of the body node from its 0 position measured as blocks, type double
+     * @param tailNodeOffset               The offset of the tail node from its 0 position measured as blocks, type double
+     * @param maxNodeJointAngle            The maximum angle a node can rotate in degrees, type double
+     * @param yAxisInterpolation           The interpolation of the Y-axis, type double
+     * @param turningSplineInterpolation   The interpolation of the turning spline to prevent overturning, type double
+     * @param passiveSineWaveSpeed         The speed of the passive sine wave animation, type double
+     * @param passiveSineWaveAmplitude     The amplitude of the passive sine wave animation, type double
+     * @param headModel                    The model used for the head node, type net.jeqo.bloons.balloon.multipart.MultipartBalloonModel
+     * @param bodyModel                    The model used for the body node, type net.jeqo.bloons.balloon.multipart.MultipartBalloonModel
+     * @param tailModel                    The model used for the tail node, type net.jeqo.bloons.balloon.multipart.MultipartBalloonModel
+     */
     public MultipartBalloonType(String id, String permission, String name, String[] lore, int nodeCount, double distanceBetweenNodes, double headNodeOffset, double bodyNodeOffset, double tailNodeOffset, double maxNodeJointAngle, double yAxisInterpolation, double turningSplineInterpolation, double passiveSineWaveSpeed, double passiveSineWaveAmplitude, MultipartBalloonModel headModel, MultipartBalloonModel bodyModel, MultipartBalloonModel tailModel) {
         this.setId(id);
         this.setPermission(permission);
@@ -69,6 +111,26 @@ public class MultipartBalloonType {
         this.setTailModel(tailModel);
     }
 
+    /**
+     *                                    Creates a new multipart balloon type which contains
+     *                                    the data in the configuration for the balloon
+     * @param id                          The id of the balloon, type java.lang.String
+     * @param permission                  The permission required to use the balloon (i.e. blue.jeqo), type java.lang.String
+     * @param name                        The name of the balloon, type java.lang.String
+     * @param lore                        The lore lines of the balloon in the balloon GUI, type java.lang.String[]
+     * @param nodeCount                   The number of nodes, or model, in the balloon, type int
+     * @param distanceBetweenNodes        The distance between each node in the balloon measured as blocks, type double
+     * @param headNodeOffset              The offset of the head node from its 0 position measured as blocks, type double
+     * @param bodyNodeOffset              The offset of the body node from its 0 position measured as blocks, type double
+     * @param tailNodeOffset              The offset of the tail node from its 0 position measured as blocks, type double
+     * @param maxNodeJointAngle           The maximum angle a node can rotate in degrees, type double
+     * @param yAxisInterpolation          The interpolation of the Y-axis, type double
+     * @param turningSplineInterpolation  The interpolation of the turning spline to prevent overturning, type double
+     * @param passiveSineWaveSpeed        The speed of the passive sine wave animation, type double
+     * @param headModel                   The model used for the head node, type net.jeqo.bloons.balloon.multipart.MultipartBalloonModel
+     * @param bodyModel                   The model used for the body node, type net.jeqo.bloons.balloon.multipart.MultipartBalloonModel
+     * @param tailModel                   The model used for the tail node, type net.jeqo.bloons.balloon.multipart.MultipartBalloonModel
+     */
     public MultipartBalloonType(String id, String permission, String name, String[] lore, int nodeCount, double distanceBetweenNodes, double headNodeOffset, double bodyNodeOffset, double tailNodeOffset, double maxNodeJointAngle, double yAxisInterpolation, double turningSplineInterpolation, double passiveSineWaveSpeed, MultipartBalloonModel headModel, MultipartBalloonModel bodyModel, MultipartBalloonModel tailModel) {
         this.setId(id);
         this.setPermission(permission);
