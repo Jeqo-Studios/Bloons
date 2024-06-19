b689b587d45b5a2858117c9cd34c7769308437ee
IanTapply22 <contact@ian-tapply.me>
2024-06-18 22:56:13 -0400

    Document all event methods
    Fixed not using lombok setters for instancing

--- src/main/java/net/jeqo/bloons/events/balloon/multipart/MultipartBalloonForceUnequipEvent.java
+++ src/main/java/net/jeqo/bloons/events/balloon/multipart/MultipartBalloonForceUnequipEvent.java
@@ -6,14 +6,21 @@ import net.jeqo.bloons.balloon.multipart.balloon.MultipartBalloon;
 import net.jeqo.bloons.events.BloonsEvent;
 import org.bukkit.entity.Player;
 
-@Getter
-@Setter
+/**
+ * An event that is called when a player force unequips a multipart balloon
+ */
+@Getter @Setter
 public class MultipartBalloonForceUnequipEvent extends BloonsEvent {
     private Player player;
     private MultipartBalloon balloon;
 
+    /**
+     *                 Constructor to trigger the MultipartBalloonForceUnequipEvent
+     * @param player   The player that is unequipping the balloon, type org.bukkit.entity.Player
+     * @param balloon  The balloon that is being force unequipped, type net.jeqo.bloons.balloon.multipart.balloon.MultipartBalloon
+     */
     public MultipartBalloonForceUnequipEvent(Player player, MultipartBalloon balloon) {
-        this.player = player;
-        this.balloon = balloon;
+        this.setPlayer(player);
+        this.setBalloon(balloon);
     }
 }
--- src/main/java/net/jeqo/bloons/events/balloon/single/SingleBalloonEquipEvent.java
+++ src/main/java/net/jeqo/bloons/events/balloon/single/SingleBalloonEquipEvent.java
@@ -5,17 +5,29 @@ import lombok.Setter;
 import net.jeqo.bloons.events.BloonsEvent;
 import org.bukkit.entity.Player;
 
+/**
+ * An event that is called when a player equips a single balloon
+ */
 @Getter @Setter
 public class SingleBalloonEquipEvent extends BloonsEvent {
     private Player player;
     private String balloonID;
 
+    /**
+     *                  Constructor to trigger the SingleBalloonEquipEvent
+     * @param player    The player that is equipping the balloon, type org.bukkit.entity.Player
+     * @param balloonID The ID of the balloon that is being equipped, type java.lang.String
+     */
     public SingleBalloonEquipEvent(Player player, String balloonID) {
-        this.player = player;
-        this.balloonID = balloonID;
+        this.setPlayer(player);
+        this.setBalloonID(balloonID);
     }
 
+    /**
+     *                Constructor to trigger the SingleBalloonEquipEvent
+     * @param player    The player that is equipping the balloon, type org.bukkit.entity.Player
+     */
     public SingleBalloonEquipEvent(Player player) {
-        this.player = player;
+        this.setPlayer(player);
     }
 }
