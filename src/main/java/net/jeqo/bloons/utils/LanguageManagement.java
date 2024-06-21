8ae87cb0508fcd24dc8999c16b98aa28cbf7d330
IanTapply22 <contact@ian-tapply.me>
2024-06-21 08:58:22 -0400

    Create language management method to fetch message from specified lang file

--- src/main/java/net/jeqo/bloons/utils/LanguageManagement.java
+++ src/main/java/net/jeqo/bloons/utils/LanguageManagement.java
@@ -1,6 +1,12 @@
 package net.jeqo.bloons.utils;
 
 import net.jeqo.bloons.Bloons;
+import net.jeqo.bloons.logger.Logger;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
 
 public class LanguageManagement {
 
@@ -9,5 +15,58 @@ public class LanguageManagement {
      */
     public static void copyLanguageFiles() {
         Bloons.getInstance().saveResource("languages/en_US.yml", false);
+    }
+
+    /**
+     *                  Gets a message from the language file specified in the config
+     * @param message   The message to get from the language file, type java.lang.String
+     * @return          The message from the language file, type java.lang.String/null
+     */
+    public static String getMessage(String message) {
+        File folder = new File(Bloons.getInstance().getDataFolder() + File.separator + "languages");
+
+        // Check if the folder exists
+        if (!folder.exists() || !folder.isDirectory()) {
+            Logger.logWarning("Language folder not found: " + folder.getPath());
+            return null;
+        }
+
+        // List files in the folder
+        File[] files = folder.listFiles();
+        if (files == null) {
+            Logger.logWarning("No language files in folder folder: " + folder.getPath());
+            return null;
+        }
+
+        // Process each file
+        for (File file : files) {
+            if (file.isFile()) {
+                String fileName = file.getName();
+
+                if (!fileName.startsWith(Bloons.getInstance().getConfig().getString("language"))) {
+                    continue;
+                }
+
+                // Load the configuration file
+                FileConfiguration config = YamlConfiguration.loadConfiguration(file);
+
+                // Get the configuration section
+                ConfigurationSection section = config.getConfigurationSection("");
+
+                if (section == null) {
+                    Logger.logWarning("Configuration section not found for file: " + fileName);
+                    continue;
+                }
+
+                // Process each key in the section
+                for (String key : section.getKeys(false)) {
+                    if (key.equals(message)) {
+                        return config.getString(key);
+                    }
+                }
+            }
+        }
+
+        return null;
     }
 }
