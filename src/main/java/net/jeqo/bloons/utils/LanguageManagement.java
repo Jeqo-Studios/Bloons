75c05662fa10b19a5a035d499e563b4ec480ff70
IanTapply22 <contact@ian-tapply.me>
2024-06-22 19:47:49 -0400

    Make an easy system to register new locales

--- src/main/java/net/jeqo/bloons/utils/LanguageManagement.java
+++ src/main/java/net/jeqo/bloons/utils/LanguageManagement.java
@@ -7,7 +7,13 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 /**
  * A class to manage the translations and retrieving of messages
@@ -15,18 +21,45 @@ import java.util.Objects;
 public class LanguageManagement {
 
     /**
+     * Create a new set/hashmap to store the locales to ensure that they're unique
+     */
+    private static final Set<String> availableLanguages = new HashSet<>();
+
+    static {
+        // Initialize available languages
+        String[] languages = {
+                "af_ZA", "ar_AE", "ar_BH", "ar_DZ", "ar_EG", "ar_IQ", "ar_JO", "ar_KW", "ar_LB", "ar_LY", "ar_MA",
+                "ar_MO", "ar_QA", "ar_SA", "ar_SY", "ar_TN", "ar_YE", "az_AZ", "be_BY", "bg_BG", "bn_IN", "bs_BA",
+                "ca_ES", "cs_CZ", "da_DK", "de_AT", "de_CH", "de_DE", "de_LI", "de_LU", "el_GR", "en_AU", "en_BZ",
+                "en_CA", "en_GB", "en_IE", "en_JM", "en_NZ", "en_PH", "en_TT", "en_US", "en_VI", "en_ZA", "en_ZW",
+                "es_US", "et_EE", "eu_ES", "fi_FL", "fo_FO", "fr_BE", "fr_CA", "fr_CH", "fr_FR", "fr_LU", "fr_MC",
+                "gl_ES", "gu_IN", "he_IL", "hi_IN", "hr_HR", "hu_HU", "hy_AM", "id_ID", "is_IS", "it_CH", "it_IT",
+                "ja_JP", "ka_GE", "kk_KZ", "kn_IN", "ko_KR", "kok_IN", "lt_LT", "lv_LV", "mk_MK", "ml_IN", "mr_IN",
+                "ms_BN", "ms_MY", "mt_MT", "nl_BE", "nl_NL", "pt_PT", "ru_RU", "sq_AL", "zh_CN", "zh_MO", "zh_SG",
+                "zh_TW"
+        };
+        // Add all languages to the set
+        Collections.addAll(availableLanguages, languages);
+    }
+
+    /**
      * Copies all language files over from the languages directory
      */
     public static void copyLanguageFiles() {
-        Bloons.getInstance().saveResource(ConfigConfiguration.LANGUAGES_CONFIGURATION_FOLDER + "/en_US.yml", false);
-        Bloons.getInstance().saveResource(ConfigConfiguration.LANGUAGES_CONFIGURATION_FOLDER + "/es_US.yml", false);
-        Bloons.getInstance().saveResource(ConfigConfiguration.LANGUAGES_CONFIGURATION_FOLDER + "/fr_FR.yml", false);
-        Bloons.getInstance().saveResource(ConfigConfiguration.LANGUAGES_CONFIGURATION_FOLDER + "/ja_JP.yml", false);
-        Bloons.getInstance().saveResource(ConfigConfiguration.LANGUAGES_CONFIGURATION_FOLDER + "/ko_KR.yml", false);
-        Bloons.getInstance().saveResource(ConfigConfiguration.LANGUAGES_CONFIGURATION_FOLDER + "/pt_PT.yml", false);
-        Bloons.getInstance().saveResource(ConfigConfiguration.LANGUAGES_CONFIGURATION_FOLDER + "/ru_RU.yml", false);
-        Bloons.getInstance().saveResource(ConfigConfiguration.LANGUAGES_CONFIGURATION_FOLDER + "/zh_CN.yml", false);
-        Bloons.getInstance().saveResource(ConfigConfiguration.LANGUAGES_CONFIGURATION_FOLDER + "/de_DE.yml", false);
+        for (String language : availableLanguages) {
+            if (!Files.exists(getLanguageFilePath(language))) {
+                Bloons.getInstance().saveResource(ConfigConfiguration.LANGUAGES_CONFIGURATION_FOLDER + File.separator + language + ".yml", false);
+            }
+        }
+    }
+
+    /**
+     *                  Gets the path to a language file
+     * @param language  The language to get the path for, type java.lang.String
+     * @return          The path to the language file, type java.nio.file.Path
+     */
+    private static Path getLanguageFilePath(String language) {
+        return Paths.get(Bloons.getInstance().getDataFolder() + File.separator + ConfigConfiguration.LANGUAGES_CONFIGURATION_FOLDER + File.separator + language + ".yml");
     }
 
     /**
